081c39b90bf49d9e886796d2a8f5bc8fb27786cb
hedi <hedihamida98@gmail.com>
2022-09-02 15:06:21 +0100

    new

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,13 @@
+
+#include "binary_trees.h"
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+
+	binary_tree_postorder(tree->right, func);
+
+	printf("n=%d\n", tree->n);
+}
