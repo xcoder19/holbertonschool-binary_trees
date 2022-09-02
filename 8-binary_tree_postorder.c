c7397c041a69421ac213a8785fcf07d8c859bbc7
hedi <hedihamida98@gmail.com>
2022-09-02 15:07:46 +0100

    new

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,5 +1,11 @@
 
 #include "binary_trees.h"
+/**
+ * binary_tree_postorder - travers postorder
+ * @tree: tree
+ * @func: func
+ */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
