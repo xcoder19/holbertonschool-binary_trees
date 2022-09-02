92868095927326ff7c69cd54798bae3b64f3a23d
hedi <hedihamida98@gmail.com>
2022-09-02 12:56:00 +0100

    new

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *parent_left = parent->left;
+	if (node == NULL || parent == NULL)
+		return NULL;
+
+	node->n = value;
+	node->right = NULL;
+	node->parent = parent;
+
+	node->left = parent_left;
+	parent->left = node;
+
+	return node;
+}
