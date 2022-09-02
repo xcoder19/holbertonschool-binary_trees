8c67b8771ac1be2da06426e93033a5f2f400cc9b
hedi <hedihamida98@gmail.com>
2022-09-02 15:24:19 +0100

    new

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int leftHight, rightHight;
+	if (tree == NULL)
+		return (0);
+
+	leftHight = binary_tree_height(tree->left);
+	rightHight = binary_tree_height(tree->right);
+
+	return (MAX(leftHight, rightHight) + 1);
+}
