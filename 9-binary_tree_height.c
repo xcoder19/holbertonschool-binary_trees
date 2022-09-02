f6a6b3647ecdc495b55cfe0a7248ecd9d256a836
hedi <hedihamida98@gmail.com>
2022-09-02 15:27:28 +0100

    new

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,5 +10,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	leftHight = binary_tree_height(tree->left);
 	rightHight = binary_tree_height(tree->right);
 
-	return (1 + MAX(leftHight, rightHight));
+	return (0 + MAX(leftHight, rightHight));
 }
