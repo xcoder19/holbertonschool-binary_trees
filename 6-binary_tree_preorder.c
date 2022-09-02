947765fbaf6db922c4e9837e302c2dc639a1f430
hedi <hedihamida98@gmail.com>
2022-09-02 14:47:58 +0100

    new

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,8 +1,12 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_preorder - travese tree with preorder
+ * @tree: tree
+ * @func: function pointer
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 
 	printf("n=%d\n", tree->n);
