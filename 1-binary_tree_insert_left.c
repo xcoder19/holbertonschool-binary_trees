25d1780eec309fddaef5aab76e8bfe8a23868dc9
hedi <hedihamida98@gmail.com>
2022-09-02 13:02:24 +0100

    new

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,6 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	node->left = parent_left;
 	parent->left = node;
-
-	return (node);
+	free(parent_left);
+	free(node);
+	return (parent->left);
 }
