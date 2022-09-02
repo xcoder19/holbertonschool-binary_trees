05a6c817ec261417b3ac8fc9b44673b93ea7e603
hedi <hedihamida98@gmail.com>
2022-09-02 11:46:03 +0100

    new

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 
 	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 	node->parent = parent;
 
 	return (node);
