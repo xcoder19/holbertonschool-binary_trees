d8757a5e2da47f1034d3e72378feac929c0387a0
hedi <hedihamida98@gmail.com>
2022-09-02 15:01:49 +0100

    new

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,9 +10,9 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_inorder(tree->left);
+	binary_tree_inorder(tree->left, func);
 
 	printf("n=%d\n", tree->n);
 
-	binary_tree_inorder(tree->right);
+	binary_tree_inorder(tree->right, func);
 }
