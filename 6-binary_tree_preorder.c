502b6e4b0bc771599f9e484bcc1bc058b41c2b4c
hedi <hedihamida98@gmail.com>
2022-09-02 14:45:53 +0100

    new

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,7 +5,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL)
 		return;
 
-	printf("n=%d", tree->n);
+	printf("n=%d\n", tree->n);
 	binary_tree_preorder(tree->left, func);
 
 	binary_tree_preorder(tree->right, func);
