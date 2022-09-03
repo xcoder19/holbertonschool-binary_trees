f7eec8f0d19c62e67b300e8470979bf124fe262c
hedi <hedihamida98@gmail.com>
2022-09-03 20:15:27 +0100

    new

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - count balance
+ * @tree: tree
+ * Return: 1 or 0
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1);
+}
