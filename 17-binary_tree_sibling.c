ca76bb56dcbe75a9f95d4e915610e9d78ad2ac16
hedi <hedihamida98@gmail.com>
2022-09-03 19:23:38 +0100

    new

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,10 +10,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent->left == node && node->parent->right != NULL)
-		return node->parent->right;
+		return (node->parent->right);
 
 	if (node->parent->right == node && node->parent->left != NULL)
-		return node->parent->left;
+		return (node->parent->left);
 
 	return (NULL);
 }
