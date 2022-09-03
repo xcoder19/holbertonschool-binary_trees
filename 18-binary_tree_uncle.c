5cdd8363eec18f096849655ad4331d3d49874354
hedi <hedihamida98@gmail.com>
2022-09-03 20:07:33 +0100

    new

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - finds node's uncle
+ * @node: node
+ * Return: node's uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent->parent->left != NULL && node->parent->parent->left == node->parent && node->parent->parent->right != NULL)
+		return node->parent->parent->right;
+
+	if (node->parent->parent->right != NULL && node->parent->parent->right == node->parent && node->parent->parent->left != NULL)
+		return node->parent->parent->left;
+
+	return (NULL);
+}
