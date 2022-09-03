c5603f4b190baf42c5eb93fb0fad0f12bb08220c
hedi <hedihamida98@gmail.com>
2022-09-03 20:07:58 +0100

    new

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,10 +11,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent->parent->left != NULL && node->parent->parent->left == node->parent && node->parent->parent->right != NULL)
-		return node->parent->parent->right;
+		return (node->parent->parent->right);
 
 	if (node->parent->parent->right != NULL && node->parent->parent->right == node->parent && node->parent->parent->left != NULL)
-		return node->parent->parent->left;
+		return (node->parent->parent->left);
 
 	return (NULL);
 }
