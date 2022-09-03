1aa7bcbe97c5ace61fee3088dcd51bb4be7feed9
hedi <hedihamida98@gmail.com>
2022-09-03 12:28:59 +0100

    new

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+
+
+#include "binary_trees.h"
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	int count;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	count = binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right);
+
+	return (count);
+}
