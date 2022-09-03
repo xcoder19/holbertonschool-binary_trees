d6df0fe6f4337595124e01d10f3f18f7285e9d33
hedi <hedihamida98@gmail.com>
2022-09-03 20:19:45 +0100

    new

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - check if tree is perfect
+ * @tree: treee
+ * Return: 1 or 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (0);
+}
